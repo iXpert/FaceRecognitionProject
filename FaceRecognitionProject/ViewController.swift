a425fd668f895d767f110c0add1be4dd12fd2a8a
Amr ElNagar <amr.elnagar@ixpert.org>
2022-11-10 11:38:32 +0100

    Initial Commit

---
+++ FaceRecognitionProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceRecognitionProject
+//
+//  Created by Amr Elnagar on 10/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
